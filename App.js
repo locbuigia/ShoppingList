398984f72b8c55a5b56fae70df68b2df4081cfac
locbuigia <locbuig@gmail.com>
2020-02-20 12:26:42 -0800

    first

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+import {uuid} from 'uuidv4';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Steak'},
+    {id: uuid(), text: 'Egg'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    if (items.find(item => item.text === text)) {
+      Alert.alert('Error', 'Duplicate Item', {text: 'OK'});
+    } else if (!text) {
+      Alert.alert('Error', 'Please Enter an Item', {text: 'OK'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
